4a4c9c8d81993b0974a2808ba8c47c1691eff2e8
Manideep Malyala <42274353+manideep-malyala@users.noreply.github.com>
2022-03-04 02:46:15 +0530

    array practice

--- level_one/find duplicates in array.py
+++ level_one/find duplicates in array.py
@@ -1,3 +1,5 @@
+# https://practice.geeksforgeeks.org/problems/find-duplicates-in-an-array/1/?page=1&category[]=Arrays&sortBy=submissions
+
 def duplicates(self, arr, n): 
     model = { x:0 for x in range(n)}
     for ele in arr:
@@ -5,7 +7,6 @@ def duplicates(self, arr, n):
     res = []
     for key in model:
         if model[key]>1:
-           res.append(key)
-        
+           res.append(key)  
     return res if len(res)>0 else [-1]
   
